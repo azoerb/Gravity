8f8225d04df18d1b36ffea81a441ef4176e5dc9c
Andrew Zoerb <azoerb@door-6.com>
2011-10-28 12:20:57 -0500

    Trying to get collision detection working.

--- objects/LevelObject.js
+++ objects/LevelObject.js
@@ -9,7 +9,13 @@ function LevelObject(isTextured, isHudObject, textureImg) {
 	this.isLit = false;
     this.shininess = 40;
     
-
+    this.localX = 0;
+    this.localY = 1;
+    this.localZ = 2;
+    
+    this.invertX = 1;
+    this.invertY = 1;
+    this.invertZ = 1;
 
     this.texture;
     this.textureImg = textureImg;
